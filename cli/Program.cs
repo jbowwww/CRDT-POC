05a6cdb1f046bfb1c4f16ee4f38d9d9761aba935
Joely <jk@debian-BULLSEYE-live-builder-AMD64>
2022-12-06 16:58:47 +0800

    Fixed code

--- cli/Program.cs
+++ cli/Program.cs
@@ -1,26 +1,32 @@
 using System;
 using Ycs;
 
-var doc1 = new YDoc(/*new YDocOptions() {}*/);
-var doc2 = new YDoc();
-
-EventHandler<(byte[], object, Transaction)> UpdateHandler(YDoc doc) =>
-    (object? sender, (byte[] data, object origin, Transaction transaction) eventArgs) =>
+public class AemoCrdtPoc
+{
+    public static void Main()
     {
-        bool local = eventArgs.transaction.Origin == doc;
-        if (local)
-        {
-            doc.ApplyUpdateV2(eventArgs.data, eventArgs.transaction.Origin, local);
-        }
-        Console.WriteLine((doc == doc1 ? "doc1" : "doc2") + ".UpdateHandler(): eventArgs={eventArgs}");
-    };
+        var doc1 = new YDoc(/*new YDocOptions() {}*/);
+        var doc2 = new YDoc();
 
-Console.WriteLine("doc1={doc1}\ndoc2={doc2}");
+        EventHandler<(byte[], object, Transaction)> UpdateHandler(YDoc doc) =>
+            (object? sender, (byte[] data, object origin, Transaction transaction) eventArgs) =>
+            {
+                bool local = eventArgs.transaction.Origin == doc;
+                if (local)
+                {
+                    doc.ApplyUpdateV2(eventArgs.data, eventArgs.transaction.Origin, local);
+                }
+                Console.WriteLine((doc == doc1 ? "doc1" : "doc2") + ".UpdateHandler(): eventArgs={eventArgs}");
+            };
 
-doc1.GetMap().Set("name", "Document #1");
-doc1.UpdateV2 += UpdateHandler(doc2);
+        Console.WriteLine("doc1={doc1}\ndoc2={doc2}");
 
-doc2.GetMap().Set("name", "Document #2");
-doc2.UpdateV2 += UpdateHandler(doc1);
+        doc1.GetMap().Set("name", "Document #1");
+        doc1.UpdateV2 += UpdateHandler(doc2);
 
-Console.WriteLine("doc1={doc1}\ndoc2={doc2}");
+        doc2.GetMap().Set("name", "Document #2");
+        doc2.UpdateV2 += UpdateHandler(doc1);
+
+        Console.WriteLine("doc1={doc1}\ndoc2={doc2}");
+    }
+}
