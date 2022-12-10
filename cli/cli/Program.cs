a0d72cbd19ee2086f8486f2b06825553432f94e8
Joel Bowman <jbowwww@gmail.com>
2022-12-10 12:05:41 +0800

    Added custom YDocEx class to enfore root doc being a map, and YMapExtensions.ToString for outputting map entries

--- cli/cli/Program.cs
+++ cli/cli/Program.cs
@@ -1,14 +1,42 @@
 using System;
+using System.Text;
 using Ycs;
+
+public static class YMapExtensions
+{
+    public static string ToString(this YMap map)
+    {
+        StringBuilder sb = new StringBuilder("\n");
+        foreach (var entry in map)
+        {
+            sb.Append($"\t{entry.Key}={entry.Value}");
+        }
+        return sb.ToString();
+    }
+}
+
+public class YDocEx : YDoc
+{
+    public YMap Get() => this.GetMap();
+
+    public new TValue? Get<TValue>(string name = "") where TValue : class => base.GetMap().Get(name) as TValue;
+    
+    public void Set(string name, object value) => base.GetMap().Set(name, value);
+
+    public override string ToString()
+    {
+        return $"[{this.GetType()}: {YMapExtensions.ToString(this.GetMap())}]";
+    }
+}
 
 public class AemoCrdtPoc
 {
     public static void Main()
     {
-        var doc1 = new YDoc(/*new YDocOptions() {}*/);
-        var doc2 = new YDoc();
+        var doc1 = new YDocEx(/*new YDocOptions() {}*/);
+        var doc2 = new YDocEx();
 
-        EventHandler<(byte[], object, Transaction)> UpdateHandler(YDoc doc) =>
+        EventHandler<(byte[], object, Transaction)> UpdateHandler(YDoc doc, string docName = "AnonymousDoc") =>
             (object? sender, (byte[] data, object origin, Transaction transaction) eventArgs) =>
             {
                 bool local = eventArgs.transaction.Origin == doc;
@@ -16,17 +44,22 @@ public class AemoCrdtPoc
                 {
                     doc.ApplyUpdateV2(eventArgs.data, eventArgs.transaction.Origin, local);
                 }
-                Console.WriteLine((doc == doc1 ? "doc1" : "doc2") + ".UpdateHandler(): eventArgs={eventArgs}");
+                Console.WriteLine($"{docName}.UpdateHandler(): eventArgs={eventArgs}");
             };
 
-        Console.WriteLine("doc1={doc1}\ndoc2={doc2}");
+        Console.WriteLine($"doc1={doc1}\ndoc2={doc2}");
 
-        doc1.GetMap().Set("name", "Document #1");
-        doc1.UpdateV2 += UpdateHandler(doc2);
+        doc1.Set("name", "Document #1");
+        doc1.UpdateV2 += UpdateHandler(doc2, nameof(doc1));
 
-        doc2.GetMap().Set("name", "Document #2");
-        doc2.UpdateV2 += UpdateHandler(doc1);
+        doc2.Set("name", "Document #2");
+        doc2.UpdateV2 += UpdateHandler(doc1, nameof(doc2));
 
-        Console.WriteLine("doc1={doc1}\ndoc2={doc2}");
-    }
-}
+        Console.WriteLine($"doc1={doc1}\ndoc2={doc2}");
+
+        doc1.Set("prop1", "stringValue1");
+        doc2.Set("prop2", "stringValue2");
+
+        Console.WriteLine($"doc1={doc1}\ndoc2={doc2}");
+
+    }}
