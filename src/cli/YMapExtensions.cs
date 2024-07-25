3cd5701218f32d91f5794d2afe18ee4dbf3caf9a
Joel Bowman <jbowwww@gmail.com>
2024-07-25 16:20:06 +0800

    Clean up YMapExtensions

--- src/cli/YMapExtensions.cs
+++ src/cli/YMapExtensions.cs
@@ -7,13 +7,6 @@ namespace cli;
 
 public static class YMapExtensions
 {
-  public static string ToString(this YMap map, string? suffix = null) => string.Join("\n\t", map.AsEnumerable().Select(entry => $"{entry.Key}={entry.Value}")) + suffix;
-  // {
-  //   var sb = new StringBuilder("");//("\n");
-  //   foreach (var entry in map)
-  //   {
-  //     sb.Append($"\n\t{entry.Key}={entry.Value}");
-  //   }
-  //   return sb.ToString();
-  // }
+  public static string ToString(this YMap map, string? suffix = null) =>
+    string.Join("\n\t", map.AsEnumerable().Select(entry => $"{entry.Key}={entry.Value}")) + suffix;
 }
